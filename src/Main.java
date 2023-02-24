a680f50bc028f649eaee4a45e5072e2e40a3ca75
Amee <ameecova@yahoo.com>
2023-02-25 00:41:25 +0100

    Create cupcake class variables and functions

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main{
     public static void main(String[] args){
-        
+
+    }
+    class Cupcake{
+        public double price;
+
+        public double getPrice;
+
+        public void setPrice(double price){
+            this.price = price;
+        }
+
+        public void type(){
+            System.out.println("A basic, generic cupcake, with vanilla frosting");
+        }
     }
 }
