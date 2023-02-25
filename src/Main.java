2b0bd1cec8a6907f5287028c2611f74a0c4c0f88
Amee <ameecova@yahoo.com>
2023-02-25 14:33:21 +0100

    Fix add main method

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,44 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args){
+        ArrayList<Cupcake> cupcakeMenu = new ArrayList<Cupcake>();
 
-    }
+        Cupcake cupcake = new Cupcake();
 
-    class Cupcake{
-        public double price;
+        RedVelvet redVelvet = new RedVelvet();
 
-        public double getPrice;
-
-        public void setPrice(double price){
-            this.price = price;
-        }
-
-        public void type(){
-            System.out.println("A basic, generic cupcake, with vanilla frosting");
-        }
-    }
-    class RedVelvet extends Cupcake {
-        public void type() {
-            System.out.println("A red velvet based cupcake, with cream cheese frosting.");
-        }
-
-    }
-    class Chocolate extends Cupcake {
-        public void type (){
-            System.out.println("A chocolate based cupcake, with chocolate frosting");
-        }
+        Chocolate chocolate = new Chocolate();
         
+        System.out.println("We are in the middle of creating the cupcake menu. We currently have three cupcakes on the menu but we need to decide on pricing.");
+        Scanner input = new Scanner(System.in);
+        System.out.println("\nWe are deciding on the price for our standard cupcake. Here is the description:");
+        cupcake.type();
     }
+}
+class Cupcake{
+    public double price;
+
+    public double getPrice;
+
+    public void setPrice(double price){
+        this.price = price;
+    }
+
+    public void type(){
+        System.out.println("A basic, generic cupcake, with vanilla frosting");
+    }
+}
+class RedVelvet extends Cupcake {
+    public void type() {
+        System.out.println("A red velvet based cupcake, with cream cheese frosting.");
+    }
+
+}
+class Chocolate extends Cupcake {
+    public void type (){
+        System.out.println("A chocolate based cupcake, with chocolate frosting");
+    }
+    
 }
