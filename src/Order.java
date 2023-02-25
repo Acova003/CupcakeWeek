5e5cde2084b45025f907358debdce1d434009084
Amee <ameecova@yahoo.com>
2023-02-25 20:59:33 +0100

    Create list of objects named order and prompt user

--- src/Order.java
+++ src/Order.java
@@ -1,8 +1,23 @@
 import java.lang.reflect.Constructor;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Order {
     public Order(ArrayList<Cupcake> cakeMenu, ArrayList<Drink>drinkMenu){
+        System.out.println("Hello customer. Would you like to place an order? (Y or N)");
+
+        ArrayList<Object> order = new ArrayList<Object>();
+
+        Scanner input = new Scanner(System.in);
+
+        String placeOrder = input.nextLine();
+
+        if (placeOrder.equalsIgnoreCase("Y")) {
+            
+        } else {
+            System.out.println("Have a nice day then");
+        }
+
 
     }
 }
