052de690e49d8882c420d3a5df4ad8bf5a521f06
Amee <ameecova@yahoo.com>
2023-02-25 14:14:50 +0100

    Create cupcake objects

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
+import java.util.ArrayList;
+
 public class Main{
     public static void main(String[] args){
 
     }
+
     class Cupcake{
         public double price;
 
@@ -25,5 +28,6 @@ public class Main{
         public void type (){
             System.out.println("A chocolate based cupcake, with chocolate frosting");
         }
+        
     }
 }
