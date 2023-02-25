310469b9ce211887f3ae2d0bb282e6aec76d7a3c
Amee <ameecova@yahoo.com>
2023-02-25 13:19:02 +0100

    Create RedVelvet and Chocolate classes

--- src/Main.java
+++ src/Main.java
@@ -15,4 +15,15 @@ public class Main{
             System.out.println("A basic, generic cupcake, with vanilla frosting");
         }
     }
+    class RedVelvet extends Cupcake {
+        public void type() {
+            System.out.println("A red velvet based cupcake, with cream cheese frosting.");
+        }
+
+    }
+    class Chocolate extends Cupcake {
+        public void type (){
+            System.out.println("A chocolate based cupcake, with chocolate frosting");
+        }
+    }
 }
