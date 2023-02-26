004c37770acc2254e30ef3f32287552b26fde9b4
Amee <ameecova@yahoo.com>
2023-02-26 15:22:39 +0100

    Create order choice logic

--- src/Order.java
+++ src/Order.java
@@ -27,6 +27,45 @@ public class Order {
                 System.out.println(itemNumber);
                 cupcakeMenu.get(index).type();
                 System.out.println("Price: " + cupcakeMenu.get(index).getPrice());
+                System.out.println("\nDRINKS: ");
+            }
+            for (int i = 0; i < drinkMenu.size(); i++) {
+                System.out.println(i);
+                System.out.println("Price: " + drinkMenu.get(i).getPrice());
+                System.out.println();
+            }
+            boolean ordering = true;
+            while (ordering == true) {
+                System.out.println("What would you like to order? Please use the number associated with each item to order.");
+                int orderChoice = input.nextInt();
+                input.nextLine();
+
+                if (orderChoice == 1) {
+                    order.add(cupcakeMenu.get(0));
+                    System.out.println("Item added to order.");
+                } else if (orderChoice == 2){
+                    order.add(cupcakeMenu.get(1));
+                    System.out.println("Item added to order.");
+                } else if (orderChoice == 3){
+                    order.add(cupcakeMenu.get(2));
+                    System.out.println("Item added to order.");
+                } else if (orderChoice == 4){
+                    order.add(cupcakeMenu.get(3));
+                    System.out.println("Item added to order.");
+                } else if (orderChoice == 5){
+                    order.add(cupcakeMenu.get(4));
+                    System.out.println("Item added to order.");
+                } else if (orderChoice == 6){
+                    order.add(cupcakeMenu.get(5));
+                    System.out.println("Item added to order.");
+                } else {
+                    System.out.println("Sorry, we don’t seem to have that on the menu.");
+                    System.out.println("Would you like to continue ordering? (Y/N)");
+                    String continueOrder = input.nextLine();
+                    if (!continueOrder.equalsIgnoreCase("Y")) {
+                        ordering = false;
+                    }
+                }
             }
         } else {
             System.out.println("Have a nice day then");
